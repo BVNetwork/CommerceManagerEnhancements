526e7f9bf8eea4bc77a1beeac4745e51fc07b2f3
Petter Sørby <sorby@bvnetwork.no>
2015-04-09 10:26:43 +0200

    Added System.Web to HtmlEncode Customer Name

--- src/EPiCode.CommerceManager.Enhancements/Order/OrderSearch.ascx.cs
+++ src/EPiCode.CommerceManager.Enhancements/Order/OrderSearch.ascx.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 using System.Web.UI.WebControls;
 using EPiServer.ServiceLocation;
 using Mediachase.BusinessFoundation;
