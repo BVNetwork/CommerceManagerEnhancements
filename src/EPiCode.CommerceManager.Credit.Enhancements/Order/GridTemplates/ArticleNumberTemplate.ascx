e59b5e4cb55482678de946992f57497d9cba2fc4
Petter Sørby <petter@soerby.com>
2017-05-02 00:27:00 +0200

    Add article number to Item List in Commerce Manager

---
+++ src/EPiCode.CommerceManager.Credit.Enhancements/Order/GridTemplates/ArticleNumberTemplate.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ArticleNumberTemplate.ascx.cs" Inherits="CommerceManagerCreditEnhancements.Order.GridTemplates.ArticleNumberTemplate" %>
+
+
+<asp:Label runat="server" ID="ArticleNumber"></asp:Label>
---
+++ src/EPiCode.CommerceManager.Credit.Enhancements/Order/GridTemplates/ArticleNumberTemplate.ascx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Mediachase.Commerce;
+using Mediachase.Commerce.Orders;
+using Mediachase.MetaDataPlus;
+using Mediachase.Web.Console.BaseClasses;
+using Mediachase.Web.Console.Interfaces;
+
+namespace CommerceManagerCreditEnhancements.Order.GridTemplates
+{
+    public partial class ArticleNumberTemplate : BaseUserControl, IEcfListViewTemplate
+    {
+        
+        private object _DataItem;
+
+
+        public override void DataBind()
+        {
+            base.DataBind();
+            LineItem dataItem = this.DataItem as LineItem;
+
+            if (dataItem != null)
+            {
+                ArticleNumber.Text = dataItem["ArticleNumber"] != null
+                  ? dataItem["ArticleNumber"].ToString()
+                  : "---";
+            }
+            else if (this.DataItem is DataRowView)
+            {
+                DataRowView dataRowView = this.DataItem as DataRowView;
+                var items = LineItem.Load(MetaDataContext.Instance, (int) dataRowView["LineItemId"], "LineItemEx");
+                ArticleNumber.Text = items["ArticleNumber"] != null ? items["ArticleNumber"].ToString() : "---";
+            }
+            else
+            {
+                ArticleNumber.Text = "---";
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public object DataItem
+        {
+            get
+            {
+                return this._DataItem;
+            }
+            set
+            {
+                this._DataItem = value;
+            }
+        }
+    }
+}
